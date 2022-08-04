1f9b9729089ae5b2e41ca7f3b6d2bd7876686839
Mahmoud Abdallah <mah64725@gmail.com>
2022-08-04 14:26:46 +0200

    add logs

--- techtrends/app.py
+++ techtrends/app.py
@@ -1,25 +1,29 @@
 import sqlite3
-import logging
-from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
+
+from flask import Flask, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+from datetime import datetime
+import logging
 
-db_connection_count = 0
+# Count all database connections
+connection_count = 0
 
 
+# Function to get a database connection.
+# This function connects to database with the name `database.db`
 def get_db_connection():
-    global db_connection_count
+    global connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
-    db_connection_count += 1
+    connection_count += 1
     return connection
 
+
 # Function to get a post using its ID
-
-
 def get_post(post_id):
     connection = get_db_connection()
-    post = connection.execute(
-        'SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    post = connection.execute('SELECT * FROM posts WHERE id = ?',
+                              (post_id, )).fetchone()
     connection.close()
     return post
 
@@ -29,36 +33,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
 
-@app.route('/healthz')
-def health_check():
-    response = app.response_class(
-        response=json.dumps({"result": "OK - healthy"}),
-        status=200,
-        mimetype='application/json'
-    )
-
-    # log line
-    app.logger.info('healthz  request successfull')
-    return response
-
-
-@app.route('/metrics')
-def metrics():
-    connection = get_db_connection()
-    posts = connection.execute('SELECT COUNT(*) FROM posts').fetchone()[0]
-    #connections = connection.execute('SELECT COUNT(*) FROM connections')
-    response = app.response_class(
-        response=json.dumps({"status": "success", "code": 200, "data": {
-                            "db_connection_count": db_connection_count, "post_count": posts}}),
-        status=200,
-        mimetype='application/json'
-    )
-
-    # log line
-    app.logger.info('Metrics request successfull')
-    return response
-
-
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -67,23 +42,28 @@ def index():
     return render_template('index.html', posts=posts)
 
 
+# Define how each individual article is rendered
+# If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-        app.logger.info(f'No article with {post_id} Id was found')
+        log_message(
+            'Article with id "{id}" does not exist!'.format(id=post_id))
         return render_template('404.html'), 404
     else:
-        app.logger.info('The article is successfully found')
+        log_message('Article "{title}" retrieved!'.format(title=post['title']))
         return render_template('post.html', post=post)
 
 
+# Define the About Us page
 @app.route('/about')
 def about():
-    app.logger.info('About request successfully was retrieved')
+    log_message('About page rendered!')
     return render_template('about.html')
 
 
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -95,16 +75,48 @@ def create():
         else:
             connection = get_db_connection()
             connection.execute(
-                'INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+                'INSERT INTO posts (title, content) VALUES (?, ?)',
+                (title, content))
             connection.commit()
             connection.close()
-            app.logger.info(f'Article "{title}" was created successfully')
+            log_message('Article "{title}" created!'.format(title=title))
             return redirect(url_for('index'))
-
     return render_template('create.html')
+
+
+# Define healthz endpoint
+@app.route('/healthz')
+def healthz():
+    try:
+        connection = get_db_connection()
+        connection.cursor()
+        connection.execute('SELECT * FROM posts')
+        connection.close()
+        return {'result': 'OK - healthy'}
+    except Exception:
+        return {'result': 'ERROR - unhealthy'}, 500
+
+
+# Define metrics endpoint
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    post_count = len(posts)
+    data = {"db_connection_count": connection_count, "post_count": post_count}
+    return data
+
+
+# Function that logs messages
+def log_message(msg):
+    app.logger.info('{time} | {message}'.format(
+        time=datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), message=msg))
 
 
 # start the application on port 3111
 if __name__ == "__main__":
-    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    # stream logs to a file
+    logging.basicConfig(level=logging.DEBUG)
+
     app.run(host='0.0.0.0', port='3111')
